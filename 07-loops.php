ae03e0abd96eaa9d395ebfe7d20b9fe9371e15f9
Vanessa Baldin <vanbaldin08@gmail.com>
2025-02-06 09:32:25 -0300

    Inclui explicações

--- 07-loops.php
+++ 07-loops.php
@@ -6,6 +6,19 @@
   <title>PHP - 07- Loops</title>
 </head>
 <body>
-  
+  <h1>Trabalhando com comandos/laços de repetição</h1>  
+  <hr>
+
+  <h2>While (ENQUANTO)</h2>
+  <p>Executa ações <b>enquanto a condição for verdadeira</b>. Se a condição inicialmente não é verdadeira, o loop não funciona nenhuma vez.</p>
+
+
+  <H2>DO/WHILE (parecido com REPITA)</H2>
+  <p>Repete <b>pelo menos uma vez</b> as ações e, caso a condição continue verdadeira, segue fazendo outros ciclos de repetição até a condição se tornar falsa.</p>
+
+  <h2>FOR (PARA)</h2>
+  <p>Executa ações por uma <b>quantidade determinada</b> de vezes.</p>
+
+
 </body>
 </html>
