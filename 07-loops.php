ac7688732bebcc7f02aba0dc3b9937207e50f499
Vanessa Baldin <vanbaldin08@gmail.com>
2025-02-06 10:51:34 -0300

    Resolução do mini-exercicio

--- 07-loops.php
+++ 07-loops.php
@@ -57,10 +57,43 @@ for($i = 1; $i <= 10; $i++ ){
   <p><code>i</code> vale: <b><?=$i?> </b></p>
 <?php
 }
+?>
 
+      <hr>
+      <h2>Mini- exercício</h2>
+      <p>Crie um array contendo os 12 meses do ano e usando um dos loops que vimos, mostre os nomes dos meses em uma lista ordenada HTML.</p>
+      <hr>
+
+
+<?php
+$meses = [
+    "Janeiro", 
+    "Fevereiro", 
+    "Março", 
+    "Abril", 
+    "Maio", 
+    "Junho", 
+    "Julho", 
+    "Agosto", 
+    "Setembro", 
+    "Outubro", 
+    "Novembro", 
+    "Dezembro"
+];
 
 ?>
 
+<ol>
+<?php
+for($i = 0; $i < 12 ; $i++) {
+?>
+  
+      <li><?=$meses[$i]?></li>
+  
+<?php
+}
+?>
+</ol>
 
 
 
