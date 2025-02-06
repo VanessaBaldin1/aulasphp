14ebb945cf4f31a51a0a4286620e8ec02cd7b90b
Vanessa Baldin <vanbaldin08@gmail.com>
2025-02-06 08:29:59 -0300

    Exemplo operador && (E/AND)

--- 06-operadores-logicos.php
+++ 06-operadores-logicos.php
@@ -10,6 +10,19 @@
   <hr>
   
   <h2>&& (E/AND)</h2>
+  <p>Todas as condições precisam ser <b>VERDADEIRAS/TRUE</b></p>
+<?php
+/*Avaliar um aluno mediante média e faltas*/ 
+$media = 9.5;
+$faltas = 10;
+
+if ($media >= 7 && $faltas <= 10) {
+  echo "<p>Aprovado!</p>";
+} else {
+  echo "<p> Reprovado!</p>";
+}
+?>
+
 
   <!-- o símbolo | é chamado de pipe -->
   <h2>|| (OU/OR)</h2>
