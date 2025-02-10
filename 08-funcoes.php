6c9cee034a1d8f29e8dc3d4c96a5ca05a5f68ad4
Vanessa Baldin <vanbaldin08@gmail.com>
2025-02-10 11:52:28 -0300

    Adiciona exemplo 08-funcoes

--- 08-funcoes.php
+++ 08-funcoes.php
@@ -3,16 +3,29 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>PHP - Funções</title>
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   
-
 </head>
 <body>
-  
   <div class="container">
-    
+    <h1>Trabalhando com funções</h1>
+    <hr>
+
+    <h2>Função como procedimento (ou sub-rotina)</h2>
+    <p><i>Procedimentos</i> <b>não retornam</b>nada.</p>
+  <?php
+  function exibirDadosDoAutor(){
+    echo "<h4>Fulano de Tal</h4>";
+    echo "<p><time>".date("d/m/y")."</time></p>";
+  }
+  ?>
+      <h3>Chamada do procedimento</h3>
+      <div> <?=exibirDadosDoAutor()?></div>
+
+
+     <hr>
   </div>
 
 
