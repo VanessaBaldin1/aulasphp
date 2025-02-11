3c4f19898e39d57221330792bc77ff95e9dce206
Vanessa Baldin <vanbaldin08@gmail.com>
2025-02-11 09:49:38 -0300

    Refatora com if/else ternário ?:

--- 08-funcoes.php
+++ 08-funcoes.php
@@ -115,11 +115,15 @@ e que o retorno da função DEVE ser do tipo string*/
 
 
 function verificarNegativo(int $valor):string {
-  if ($valor < 0) {
-    return "é negativo";
-  } else {
-     return "não é negativo";
-  }
+  //if ($valor < 0) {
+   // return "é negativo";
+  //} else {
+    // return "não é negativo";
+  //}
+
+  //Refatora com if/else ternário ?:
+  return $valor < 0 ? "é negativo" : "não é negativo";
+
 }
 
 
