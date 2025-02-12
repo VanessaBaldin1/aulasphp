98b234f3fe4880b9261977d81b37b1bd300f8cc4
Vanessa Baldin <vanbaldin08@gmail.com>
2025-02-12 11:20:37 -0300

    correcao do exercicio 05

--- exercicios/exercicio05-array.php
+++ exercicios/exercicio05-array.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Exercício 05 (Funções) - 11/02/2025</title>
+  <title>Array</title>
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   
@@ -12,51 +12,58 @@
 <body>
   
   <div class="container">
-    
-<?php
-$nota1 = 5;
-$nota2 = 4;
-$nota3 = 1;
 
-$total = $nota1 + $nota2 + $nota3;
-
-//$media = $total/3;
-
+  <?php
+  $alunos = [
+ [ 'nome' => 'Maria',
+   'nota1'=>  5,
+   'nota2' => 8,
+   'nota3' => 9,
+  ],
+  [ 'nome' => 'Paulo',
+   'nota1'=>  10,
+   'nota2' => 8,
+   'nota3' => 9,
+  ],
+  [ 'nome' => 'Luiz',
+  'nota1'=>  3,
+  'nota2' => 8,
+  'nota3' => 10,
+ ],
+ [ 'nome' => 'Laura',
+ 'nota1'=>  10,
+ 'nota2' => 8,
+ 'nota3' => 9,
+],
+[ 'nome' => 'Ana',
+   'nota1'=>  10,
+   'nota2' => 10,
+   'nota3' => 9,
+  ],
+];
 ?>
 
 
 
 <?php
-$media = fn(float $total): float => $total/3;
-
-$resultado = $media($total);
-
-?>
+  foreach ($alunos as $aluno){
+ 
+     $media =($aluno['nota1'] + $aluno['nota2'] + $aluno['nota3'])/3;
+  ?>
+    <p> Aluno(a) <?=$aluno["nome"]?> teve as notas <?=$media?></p>
 
 <?php
-
-function verificar_situacao($resultado):string {
-  if ($resultado >=7) {
-   return '<span class="badge text-bg-success">Aprovado</span>';
-  } else {
-    return '<span class="badge text-bg-danger">Reprovado</span>';
   }
-
-}
-
-?>
-
-<?php
-$mediafinal = number_format($media($total), 2, ".",",");
 ?>
 
 
 
-  <p>A média do aluno tem a nota <?=$mediafinal?> o status dele é <?=verificar_situacao($resultado)?></p>
 
-  
 
+    
   </div>
+
+
 
 
 
