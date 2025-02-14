b8d99b33c611e87e441f215e40b9ab74d58ebf29
Vanessa Baldin <vanbaldin08@gmail.com>
2025-02-14 08:58:37 -0300

    Adiciona fragmento footer

--- 11-site/index.php
+++ 11-site/index.php
@@ -2,20 +2,5 @@
       <h2>Bem-vindo ao site Exemplo</h2>
       <p>Esta é primeira página do nosso site.</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, eos exercitationem nam ratione sequi et sunt dolorem quasi similique temporibus ad animi molestias facere neque!</p>
-  </main>
-
-  <footer>
-      <p>Site criado para o curso Téc.Informática para Internet</p>
-  </footer>
-  </div>
-
-
-
-
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
- 
-
-
-</body>
-</html>
+  
+<?php include "footer.php"?>
