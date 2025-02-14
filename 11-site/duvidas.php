f06925f0690a8fc13202163d0e550b215c2f741c
Vanessa Baldin <vanbaldin08@gmail.com>
2025-02-14 08:37:13 -0300

    Cria página de dúvidas

---
+++ 11-site/duvidas.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Site com PHP</title>
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  
+
+</head>
+<body>
+  
+  <div class="container">
+  <header>
+       <h1>Dúvidas - Site com PHP</h1>
+         <nav>
+          <a href="index.php">Home</a>
+          <a href="cursos.php">Cursos</a>
+          <a href="duvidas.php">Dúvidas</a>
+        </nav>
+  </header>
+
+  <main>
+      <h2>Página de Dúvidas</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius atque repellat nam possimus velit? Necessitatibus eveniet ducimus animi nostrum. Dolorum cumque quis, beatae omnis odio nisi esse distinctio, quasi est dolores deleniti hic veritatis enim.</p>
+
+      <h3>Principais dúvidas</h3>
+
+      <details>
+        <Summary>Os Cursos são Gratuitos?</Summary>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus similique natus accusamus nobis vitae commodi.</p>
+      </details>
+      
+  </main>
+
+  <footer>
+      <p>Site criado para o curso Téc.Informática para Internet</p>
+  </footer>
+  </div>
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+ 
+
+
+</body>
+</html>
